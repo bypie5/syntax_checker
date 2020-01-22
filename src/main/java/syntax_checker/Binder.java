c269affe6eaedfccf2f39a79acb59a10bb54cb34
bypie5 <bypie966@gmail.com>
2020-01-21 17:36:10 -0800

    Added constructed for binder

--- src/main/java/syntax_checker/Binder.java
+++ src/main/java/syntax_checker/Binder.java
@@ -3,4 +3,8 @@ package syntax_checker;
 public class Binder {
     public Symbol symbol;
     public Object binding;
+
+    public Binder(Symbol s, Object b) {
+        symbol = s; binding = b;
+    }
 }
