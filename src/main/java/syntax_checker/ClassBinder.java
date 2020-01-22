6508208fe4329001d149431846ba8343e7cdddff
bypie5 <bypie966@gmail.com>
2020-01-22 13:23:13 -0800

    Added fields binder class

--- src/main/java/syntax_checker/ClassBinder.java
+++ src/main/java/syntax_checker/ClassBinder.java
@@ -2,6 +2,7 @@ package syntax_checker;
 
 public class ClassBinder extends Binder {
 
+    FieldsBinder fields;
     MethodsBinder methods;
 
     public ClassBinder() {
---
+++ src/main/java/syntax_checker/FieldsBinder.java
@@ -0,0 +1,11 @@
+package syntax_checker;
+
+public class FieldsBinder extends Binder {
+
+    public FieldsBinder () {
+
+    }
+    public boolean addSymbol(Symbol key, Binder b) {
+        return false;
+    }
+}
